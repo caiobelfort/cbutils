1fed057c331d78b81a30a94dcddf77b5d5225ab2
Caio Belfort <caiobelfort90@gmail.com>
2017-08-05 11:35:29 -0300

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cbutils',
+    version='0.01',
+    description='My utilities library',
+    url='https://github.com/caiobelfort/cbutils',
+    author='Caio Belfort',
+    author_email='caiobelfort90@gmail.com',
+    license='GPL',
+    packages=['cbutils'],
+    zip_safe=False
+)
